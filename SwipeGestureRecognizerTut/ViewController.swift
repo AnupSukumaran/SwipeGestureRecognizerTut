c795598857aa69c431571acf7c9d9f5de71a8077
Anup Sukumaran <anup.sukumaran9@gmail.com>
2018-10-18 18:57:16 +0530

    Initial Commit

---
+++ SwipeGestureRecognizerTut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeGestureRecognizerTut
+//
+//  Created by Sukumar Anup Sukumaran on 18/10/18.
+//  Copyright © 2018 TechTonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
